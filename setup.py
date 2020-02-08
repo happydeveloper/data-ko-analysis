6c37558046df83342d6568245ae27b61e170cfa7
duru <enfn2001@gmail.com>
2020-02-09 02:01:21 +0900

    파이썬 패퀴지 등록

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(name='')
+setup(
+    name             = 'data-ko-analysis',
+    version          = '0.1',
+    description      = '데이타분석 파이썬을 이용한 예제들 ',
+    long_description = open('README.md').read(),
+    author           = 'duru',
+    author_email     = 'enfn2001@gmail.com',
+    url              = 'https://...',
+    download_url     = 'https://...',
+    install_requires = ['requests'],
+    packages         = find_packages(exclude = ['docs', 'example']),
+    keywords         = ['data', 'study', 'numpy'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
